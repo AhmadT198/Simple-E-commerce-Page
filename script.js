9c23ae45aec2b478877c9b583f97730c29d0c6a6
Ahmad Tamer Samir Qazamel <59650021+AhmadT198@users.noreply.github.com>
2024-03-08 00:50:29 +0200

    Adding my files

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() { 
+  const carousel = document.querySelector(".carousel"); 
+  const arrowBtns = document.querySelectorAll(".wrapper i"); 
+  const wrapper = document.querySelector(".wrapper"); 
+
+  const firstCard = carousel.querySelector(".card"); 
+  const firstCardWidth = firstCard.offsetWidth; 
+
+  // Add event listeners for the arrow buttons to  
+  // scroll the carousel left and right 
+  arrowBtns.forEach(btn => { 
+      btn.addEventListener("click", () => { 
+          carousel.scrollLeft += btn.id === "left" ?  
+              -firstCardWidth : firstCardWidth; 
+      }); 
+  }); 
+}); 
